cce99418b6c52d71cce0d9ad4fac0aa5a0be7a47
ngohoang2110 <hoanggg2110@gmail.com>
2022-09-25 11:41:06 +0700

    Authentication Authorization

--- src/main/java/com/srs/rental/RentalServiceApplication.java
+++ src/main/java/com/srs/rental/RentalServiceApplication.java
@@ -2,8 +2,10 @@ package com.srs.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RentalServiceApplication {
 
     public static void main(String[] args) {
