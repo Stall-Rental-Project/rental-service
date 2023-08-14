8518bacc4fd7a821e88aecf5cdb8ff7b5015d317
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2023-08-14 20:31:27 +0700

    Remove Eureka Client && Setup CI/CD Pipeline -> Jenkins && Docker && K8s

--- src/main/java/com/srs/rental/RentalServiceApplication.java
+++ src/main/java/com/srs/rental/RentalServiceApplication.java
@@ -2,10 +2,8 @@ package com.srs.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class RentalServiceApplication {
 
     public static void main(String[] args) {
